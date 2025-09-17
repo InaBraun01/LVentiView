d9fda4f2e55940c4193793c6229adc3a00c687de
Ina Braun <ibraun@antofalla.lfpn.ds.mpg.de>
2025-09-17 16:48:46 +0200

    latest version

--- Main_one_time.py
+++ Main_one_time.py
@@ -7,8 +7,8 @@ import csv
 import ast
 import time
 import cProfile
+import gc
 import torch
-torch.cuda.empty_cache()
 
 from Python_Code.DicomExam import DicomExam
 from Python_Code.Utilis.analysis_utils import (
@@ -30,7 +30,7 @@ local_path    = os.getcwd()
 data_dir = '/data.lfpn/ibraun/Code/paper_volume_calculation/Patient_data/'
 
 # data_dir = "/data.lfpn/ibraun/Code/paper_volume_calculation/Human_data"
-output_folder='outputs_patient_data/full_result' 
+output_folder='outputs_patient_data/final_results' 
 
 # output_folder = 'outputs_healthy_GUI'
 
@@ -41,31 +41,38 @@ output_folder='outputs_patient_data/full_result'
 # datasets = ['Welle']
 
 #Healthy datasets
-#datasets_healthy = ['SCD0003701','SCD0003801','SCD0003901','SCD0004001','SCD0004101','SCD0004201','SCD0004301','SCD0004401','SCD0004501']
-# datasets_healthy = ['SCD0004201','SCD0004301','SCD0004401','SCD0004501']
-# pathology_healthy = [ "Healthy/" for i in range(len(datasets_healthy))]
+datasets_healthy = ['SCD0003701','SCD0003801','SCD0003901','SCD0004001','SCD0004101','SCD0004201','SCD0004301','SCD0004401','SCD0004501']
+# datasets_healthy = ['SCD0003901','SCD0004101','SCD0004201','SCD0004501']
+pathology_healthy = [ "Healthy/" for i in range(len(datasets_healthy))]
 
 
-#heart failure infarct datasets #'SCD0000201',#'SCD0000601' must be run again
+#heart failure infarct datasets #'SCD0000201',#'SCD0000601', 'SCD0001701' must be run again
 #'SCD0000801' is too large can not run on this GPU. I either need to run it on the mac or I need to run it on CPU. 
-# datasets_failure_infarct = ['SCD0000101','SCD0000201','SCD0000301','SCD0000401','SCD0000501','SCD0000601', 'SCD0000701','SCD0000801','SCD0000901','SCD0001001','SCD0001101','SCD0001201']
-datasets_failure_infarct = ['SCD0000901','SCD0001001','SCD0001101','SCD0001201']
+datasets_failure_infarct = ['SCD0000101','SCD0000201','SCD0000301','SCD0000401','SCD0000501','SCD0000601', 'SCD0000701','SCD0000801','SCD0000901','SCD0001001','SCD0001101','SCD0001201']
+# datasets_failure_infarct = ['SCD0000901','SCD0001001','SCD0001101','SCD0001201']
 
 pathology_failure_infarct = ['Heart_failure_infarct/' for i in range(len(datasets_failure_infarct))]
 
 #heart failure without infarct datasets
 datasets_failure = ['SCD0001301','SCD0001401','SCD0001501','SCD0001601','SCD0001701','SCD0001801','SCD0001901','SCD0002001','SCD0002101','SCD0002201','SCD0002301','SCD0002401']
+#datasets_failure = ['SCD0001401','SCD0001501','SCD0001901','SCD0002101','SCD0002401']
 pathology_failure = ['Heart_failure/' for i in range(len(datasets_failure))]
 
 #Lv Hypertrophy datasets
 datasets_Lv = ['SCD0002501','SCD0002601','SCD0002701','SCD0002801','SCD0002901','SCD0003001','SCD0003101','SCD0003201','SCD0003301','SCD0003401','SCD0003501','SCD0003601']
+#datasets_Lv = ['SCD0002701','SCD0002801','SCD0003001','SCD0003601']
 pathology_Lv = ['LV_hypertrophy/' for i in range(len(datasets_Lv))]
 
-datasets =  datasets_failure_infarct + datasets_failure  + datasets_Lv
-pathology =  pathology_failure_infarct + pathology_failure + pathology_Lv
+datasets = datasets_healthy + datasets_failure_infarct+ datasets_failure  + datasets_Lv
+pathology = pathology_healthy + pathology_failure_infarct + pathology_failure + pathology_Lv
 
-# datasets = ['SCD0000801']
-# pathology = [ "Heart_failure_infarct/"]
+# datasets = [ 'SCD0001601','SCD0001801','SCD0002001','SCD0002501','SCD0003501' ]
+# pathology = ['Heart_failure/','Heart_failure/','Heart_failure/','LV_hypertrophy/','LV_hypertrophy/']
+
+# datasets = ['Welle']
+
+datasets = [ 'SCD0003701']
+pathology = ['Healthy/']
 
 # datasets = ['Patient077']
 # Define valid stages
@@ -109,38 +116,38 @@ for index,dataset_to_use in enumerate(datasets):
         print("Saving Automatic Segmentation Masks to Available Manual Segmentation Masks...")
         extract_auto_seg_compare_manu_seg(de)
 
-        print("Save Visualization of Segmentation Results ..")
-        #Save a visualisation of the full MRI data with the generated segmentation masks
-        de.save_images(prefix='full')
+        # print("Save Visualization of Segmentation Results ..")
+        # #Save a visualisation of the full MRI data with the generated segmentation masks
+        # de.save_images(prefix='full')
 
-        print("Estimating valve plane position...")
-        #Estimate the planes lying above the LV base
-        estimateValvePlanePosition(de)
+        # print("Estimating valve plane position...")
+        # #Estimate the planes lying above the LV base
+        # estimateValvePlanePosition(de)
 
-        print("Estimating landmarks...")
-        #Calculate landmarks
-        de.estimate_landmarks()
+        # print("Estimating landmarks...")
+        # #Calculate landmarks
+        # de.estimate_landmarks()
 
-        print("Cleaning data...")
-        #Clean MRI data based on Segmentations
-        de.clean_data()
+        # print("Cleaning data...")
+        # #Clean MRI data based on Segmentations
+        # de.clean_data()
 
-        print("Estimate MRI orientation...")
-        estimate_MRI_orientation(de)
+        # print("Estimate MRI orientation...")
+        # estimate_MRI_orientation(de)
 
-        print("Save Cleaned Visualization of Segmentation Results ..")
-        #Save a visualisation of the cleaned MRI data with the generated segmentation masks
-        de.save_images(prefix='cleaned')
+        # print("Save Cleaned Visualization of Segmentation Results ..")
+        # #Save a visualisation of the cleaned MRI data with the generated segmentation masks
+        # de.save_images(prefix='cleaned')
     
-        print("Analyse segmentation masks...")
-        compute_cardiac_parameters(de,'seg')
+        # print("Analyse segmentation masks...")
+        # compute_cardiac_parameters(de,'seg')
 
-        print("Calculate Local Thickness...")
-        seg_masks_compute_thickness_map(de)
+        # # print("Calculate Local Thickness...")
+        # # seg_masks_compute_thickness_map(de)
 
-        print("Saving analysis object...")
-        #Save object of type DicomExam as pickel file
-        de.save()
+        # print("Saving analysis object...")
+        # #Save object of type DicomExam as pickel file
+        # de.save()
 
     if stage == "meshing":
 
@@ -159,7 +166,7 @@ for index,dataset_to_use in enumerate(datasets):
             # Replace this with your actual function call
             df_end_dice = fit_mesh(
                 de,
-                training_steps=5000,
+                training_steps=5000, 
                 time_frames_to_fit="all",
                 burn_in_length=0,
                 train_mode='normal',
@@ -172,10 +179,9 @@ for index,dataset_to_use in enumerate(datasets):
             )
             end_time = time.time()
         except Exception as e:
-            print(f"Error in Dataset {dataset_to_use}: {e}")
+           print(f"Error in Dataset {dataset_to_use}: {e}")
 
-
-            # The loop continues with the next i automatically
+        # The loop continues with the next i automatically
         print(f"Myocardium Dice: {df_end_dice['Myocardium Dice'].mean():.3f}")
         print(f"Blood Pool Dice: {df_end_dice['Blood pool dice'].mean():.3f}")
         print(f"Fitting took: {end_time - start_time:.3f}s")
@@ -199,12 +205,20 @@ for index,dataset_to_use in enumerate(datasets):
         # #Calculate uncertainty of generated mesh
         # calculate_segmentation_uncertainty(de,'mesh')
 
-        print("Calculate Local Thickness...")
-        meshes_compute_thickness_map(de)
+        try:
+            print("Calculate Local Thickness...")
+            meshes_compute_thickness_map(de)
+        except Exception as e:
+           print(f"Error in Dataset {dataset_to_use}: {e}")
 
         print("Saving Analysis Object...")
         #Save object of type DicomExam as pickel file
         de.save()
+
+    # del de
+    # del df_end_dice  # if this is a DataFrame stored on GPU (unlikely, but safe)
+    torch.cuda.empty_cache()
+    gc.collect()
 
 
 
