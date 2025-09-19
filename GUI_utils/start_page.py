954cdd531c36229ade951a2e64bd763b0a3a3c49
ina.braun01@gmail.com <ibraun@LFPN-Arabka.lfpn.ds.mpg.de>
2025-09-19 13:40:14 +0200

    removed icons from start page

--- GUI_utils/start_page.py
+++ GUI_utils/start_page.py
@@ -1,6 +1,4 @@
-import os
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
-from .icon_text_button import IconTextButton  # relative import
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 
 class StartPage(QWidget):
     def __init__(self, switch_to_analysis_callback, switch_to_mesh_callback):
@@ -8,27 +6,35 @@ class StartPage(QWidget):
         self.switch_to_analysis = switch_to_analysis_callback
         self.switch_to_mesh = switch_to_mesh_callback
 
-
+        # Main vertical layout
         main_layout = QVBoxLayout()
         self.setLayout(main_layout)
         main_layout.addStretch()
+
+        # Horizontal layout for buttons
         btn_layout = QHBoxLayout()
         btn_layout.setSpacing(50)
         main_layout.addLayout(btn_layout)
         btn_layout.addStretch()
-        seg_icon_path = "segmentation_logo.png"
-        mesh_icon_path = "mesh_gen_logo.png"
-        btn_segmentation = IconTextButton(seg_icon_path, "Segmentation", size=180)
+
+        # Segmentation button
+        btn_segmentation = QPushButton("Segmentation")
+        btn_segmentation.setFixedSize(180, 180)  # square closed button
         btn_segmentation.clicked.connect(self.on_segmentation)
         btn_layout.addWidget(btn_segmentation)
-        btn_mesh_gen = IconTextButton(mesh_icon_path, "Mesh Generation", size=180)
+
+        # Mesh Generation button
+        btn_mesh_gen = QPushButton("Mesh Generation")
+        btn_mesh_gen.setFixedSize(180, 180)  # square closed button
         btn_mesh_gen.clicked.connect(self.on_mesh_generation)
         btn_layout.addWidget(btn_mesh_gen)
+
         btn_layout.addStretch()
         main_layout.addStretch()
 
     def on_segmentation(self):
         self.switch_to_analysis()
+
     def on_mesh_generation(self):
         self.switch_to_mesh()
 
