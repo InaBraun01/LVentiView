049ca905369663c38a25f87683cd5110600819af
Ina Braun <ibraun@antofalla.lfpn.ds.mpg.de>
2025-09-25 17:48:35 +0200

    added comments

--- LVentiView.py
+++ LVentiView.py
@@ -2,6 +2,10 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from GUI_utils.main_window import MainWindow
 
+# This is the main for the GUI. 
+# If you want to run the GUI from the terminal. 
+# Go into the folder that this file is in. Activate the conda envrionment and run LVentiView.py
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
